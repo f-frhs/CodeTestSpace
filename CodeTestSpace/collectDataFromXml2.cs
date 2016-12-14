a66c268ea8add6458379c292fcf70bbd83341deb
hayashi <hayashi@roboshin.com>
2016-12-14 18:54:13 +0900

    CHとFHの各穴間距離の平均と標準偏差を導出

--- CodeTestSpace/collectDataFromXml2.cs
+++ CodeTestSpace/collectDataFromXml2.cs
@@ -20,7 +20,7 @@ class Program
         var lengthX1_3 = new List<double>();
         var lengthX2_3 = new List<double>();
 
-        var placeNames = new List<string> { "CubeHole1", "CubeHole2", "CubeHole3", "FrangeHole1", "FrangeHole2", "FrangeHole3", "HoleNut1", "HoleNut2" };
+        var placeNames = new List<string> { "CubeHole1", "CubeHole2", "CubeHole3", "FrangeHole1", "FrangeHole2", "FrangeHole3", "HoleNut1", "HoleNut2", "CubeHole1_No", "CubeHole2_No", "CubeHole3_No", "FrangeHole1_No", "FrangeHole2_No", "FrangeHole3_No", "HoleNut1_No", "HoleNut2_No" };
         var pointNames = new List<string> { "X", "Y", "Z", "Orientation I", "Orientation J", "Orientation K", "Diamater" };
 
         var names = new Dictionary<string, List<double>>();
@@ -48,6 +48,7 @@ class Program
         //Console.WriteLine($"Average: {names["CubeHole1_X"].Average()}");
         //Console.WriteLine($"SD: {names["CubeHole1_X"].Sd()}");
 
+        //CH各穴間各対応座標の差の２乗
         var CH_X12 = names["CubeHole1_X"].ToArray().Zip(names["CubeHole2_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
         var CH_X13 = names["CubeHole1_X"].ToArray().Zip(names["CubeHole3_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
         var CH_X23 = names["CubeHole2_X"].ToArray().Zip(names["CubeHole3_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
@@ -60,13 +61,81 @@ class Program
         var CH_Z13 = names["CubeHole1_Z"].ToArray().Zip(names["CubeHole3_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
         var CH_Z23 = names["CubeHole2_Z"].ToArray().Zip(names["CubeHole3_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
 
+        //FH各穴間各対応座標の差の２乗
+        var FH_X12 = names["FrangeHole1_X"].ToArray().Zip(names["FrangeHole2_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_X13 = names["FrangeHole1_X"].ToArray().Zip(names["FrangeHole3_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_X23 = names["FrangeHole2_X"].ToArray().Zip(names["FrangeHole3_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var FH_Y12 = names["FrangeHole1_Y"].ToArray().Zip(names["FrangeHole2_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_Y13 = names["FrangeHole1_Y"].ToArray().Zip(names["FrangeHole3_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_Y23 = names["FrangeHole2_Y"].ToArray().Zip(names["FrangeHole3_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var FH_Z12 = names["FrangeHole1_Z"].ToArray().Zip(names["FrangeHole2_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_Z13 = names["FrangeHole1_Z"].ToArray().Zip(names["FrangeHole3_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FH_Z23 = names["FrangeHole2_Z"].ToArray().Zip(names["FrangeHole3_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        //CHN各穴間各対応座標の差の２乗
+        var CHN_X12 = names["CubeHole1_No_X"].ToArray().Zip(names["CubeHole2_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_X13 = names["CubeHole1_No_X"].ToArray().Zip(names["CubeHole3_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_X23 = names["CubeHole2_No_X"].ToArray().Zip(names["CubeHole3_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var CHN_Y12 = names["CubeHole1_No_Y"].ToArray().Zip(names["CubeHole2_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_Y13 = names["CubeHole1_No_Y"].ToArray().Zip(names["CubeHole3_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_Y23 = names["CubeHole2_No_Y"].ToArray().Zip(names["CubeHole3_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var CHN_Z12 = names["CubeHole1_No_Z"].ToArray().Zip(names["CubeHole2_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_Z13 = names["CubeHole1_No_Z"].ToArray().Zip(names["CubeHole3_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var CHN_Z23 = names["CubeHole2_No_Z"].ToArray().Zip(names["CubeHole3_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        //FHN各穴間各対応座標の差の２乗
+        var FHN_X12 = names["FrangeHole1_No_X"].ToArray().Zip(names["FrangeHole2_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_X13 = names["FrangeHole1_No_X"].ToArray().Zip(names["FrangeHole3_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_X23 = names["FrangeHole2_No_X"].ToArray().Zip(names["FrangeHole3_No_X"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var FHN_Y12 = names["FrangeHole1_No_Y"].ToArray().Zip(names["FrangeHole2_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_Y13 = names["FrangeHole1_No_Y"].ToArray().Zip(names["FrangeHole3_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_Y23 = names["FrangeHole2_No_Y"].ToArray().Zip(names["FrangeHole3_No_Y"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        var FHN_Z12 = names["FrangeHole1_No_Z"].ToArray().Zip(names["FrangeHole2_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_Z13 = names["FrangeHole1_No_Z"].ToArray().Zip(names["FrangeHole3_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+        var FHN_Z23 = names["FrangeHole2_No_Z"].ToArray().Zip(names["FrangeHole3_No_Z"].ToArray(), (x, y) => (x - y) * (x - y)).ToArray();
+
+        //CH穴間距離
         var chLength12 = CalcSD(CH_X12, CH_Y12, CH_Z12);
         var chLength13 = CalcSD(CH_X13, CH_Y13, CH_Z13);
         var chLength23 = CalcSD(CH_X23, CH_Y23, CH_Z23);
 
+        //FH穴間距離
+        var fhLength12 = CalcSD(FH_X12, FH_Y12, FH_Z12);
+        var fhLength13 = CalcSD(FH_X13, FH_Y13, FH_Z13);
+        var fhLength23 = CalcSD(FH_X23, FH_Y23, FH_Z23);
+
+        //CHN穴間距離
+        var chNLength12 = CalcSD(CHN_X12, CHN_Y12, CHN_Z12);
+        var chNLength13 = CalcSD(CHN_X13, CHN_Y13, CHN_Z13);
+        var chNLength23 = CalcSD(CHN_X23, CHN_Y23, CHN_Z23);
+
+        //FHN穴間距離
+        var fhNLength12 = CalcSD(FHN_X12, FHN_Y12, FHN_Z12);
+        var fhNLength13 = CalcSD(FHN_X13, FHN_Y13, FHN_Z13);
+        var fhNLength23 = CalcSD(FHN_X23, FHN_Y23, FHN_Z23);
+
+        //各穴間距離の平均と標準偏差
         Console.WriteLine(chLength12);
         Console.WriteLine(chLength13);
         Console.WriteLine(chLength23);
+        Console.WriteLine(fhLength12);
+        Console.WriteLine(fhLength13);
+        Console.WriteLine(fhLength23);
+
+        Console.WriteLine("--------------------------------");
+
+        Console.WriteLine(chNLength12);
+        Console.WriteLine(chNLength13);
+        Console.WriteLine(chNLength23);
+        Console.WriteLine(fhNLength12);
+        Console.WriteLine(fhNLength13);
+        Console.WriteLine(fhNLength23);
 
         Console.ReadLine();
 
